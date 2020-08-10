f97848dc122ee187366c0dd1e4eebfb606592b99
Tom Van Acker <tom.vanacker@esat.kuleuven.be>
2020-08-10 16:40:55 +0200

    Now with documentation?

--- docs/make.jl
+++ docs/make.jl
@@ -21,6 +21,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-# deploydocs(
-#      repo = "github.com/timmyfaraday/MultiStateSystems.jl.git"
-# )
+deploydocs(
+     repo = "github.com/timmyfaraday/MultiStateSystems.jl.git"
+)
