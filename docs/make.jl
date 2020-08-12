ad0f780a59ec50880913510cd4c871475851af05
Tom Van Acker <tom.vanacker@esat.kuleuven.be>
2020-08-12 15:43:29 +0200

    Did I mention small mistakes

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
                     "Getting Started"   => "quickguide.md",
                     "DSL Manual"        =>
                         [ "State-Transition Diagram"=> "std.md",
-                          "Distributions"           => "distributions.jl",
+                          "Distributions"           => "distributions.md",
                           "Generating Functions"    => "ugf.md",
                           "Network"                 => "network.md"],
                     "Models Manual"     =>
