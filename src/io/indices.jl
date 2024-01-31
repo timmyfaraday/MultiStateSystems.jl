887ed984518fc446b4b177ea9514115028ea0c9c
timmyfaraday <van_acker_tom@me.com>
2024-01-31 17:50:30 +0100

    one more time...

--- src/io/indices.jl
+++ src/io/indices.jl
@@ -6,31 +6,31 @@
 # See http://github.com/timmyfaraday/MultiStateSystems.jl                      #
 ################################################################################
 
-"""
-    EENS(usr::MultiStateSystems.PropDict)
+# """
+#     EENS(usr::MultiStateSystems.PropDict)
 
-Expected Energy Not Served (EENS) [MWh]
+# Expected Energy Not Served (EENS) [MWh]
 
-This is the amount of electricity demand of a user, measured in MWh, that is
-expected not to be met in a given year.
-    - EENS(usr) gives the EENS when an user's demand equals the maximal output
-      of the system for that user.
-"""
+# This is the amount of electricity demand of a user, measured in MWh, that is
+# expected not to be met in a given year.
+#     - EENS(usr) gives the EENS when an user's demand equals the maximal output
+#       of the system for that user.
+# """
 EENS(usr::PropDict) =
     8760u"hr"*sum((maximum(usr[:ugf].val).-usr[:ugf].val).*usr[:ugf].prb) |> u"MWh"
 
-"""
-    GRA(usr::MultiStateSystems.PropDict)
+# """
+#     GRA(usr::MultiStateSystems.PropDict)
 
-Generation Ratio Availability (GRA) [-]
+# Generation Ratio Availability (GRA) [-]
 
-This is the probability of at least transferring a specific percentage of the
-generation, Generation Ration (GR) [-], to a user through the network.
-    - GRA(usr,GR) gives the probability of transferring at least the GR to a
-      user through the network.
-    - GRA(usr) gives a user's GRA for a GR ranging from 0.0 to 1.0.
-    - GRO(usr,GR) gives the output towards a user for a given GR
-"""
+# This is the probability of at least transferring a specific percentage of the
+# generation, Generation Ration (GR) [-], to a user through the network.
+#     - GRA(usr,GR) gives the probability of transferring at least the GR to a
+#       user through the network.
+#     - GRA(usr) gives a user's GRA for a GR ranging from 0.0 to 1.0.
+#     - GRO(usr,GR) gives the output towards a user for a given GR
+# """
 GRA(usr::PropDict) = [GRA(usr,GR) for GR in 0.0:0.01:1.0]
 GRA(usr::PropDict,GR::Float64) =
     sum(usr[:ugf].prb[findfirst(x -> x .>= GRO(usr,GR),usr[:ugf].val):end])
